d4890e360b874f54d311afe9b3dd2fcd9063198c
IIIMAXIMIII <145893187+IIIMAXIMIII@users.noreply.github.com>
2025-03-11 19:09:00 +0500

    3, 4, 5, 6

--- index.js
+++ index.js
@@ -18,6 +18,30 @@ function addListeners() {
             const block = document.getElementById('scaleBlock');
             animaster().scale(block, 1000, 1.25);
         });
+
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().moveAndHide(block, 5000);
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 5000);
+        });
+
+    let stopHeartBeating;
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            stopHeartBeating = animaster().heartBeating(block);
+        });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            stopHeartBeating.stop();
+        });
 }
 
 
@@ -34,6 +58,23 @@ function getTransform(translation, ratio) {
 }
 
 function animaster() {
+    function resetFadeIn(element) {
+        element.style.transitionDuration = null;
+        element.classList.remove('hide');
+        element.classList.add('show');
+    }
+
+    function resetFadeOut(element) {
+        element.style.transitionDuration = null;
+        element.classList.remove('show');
+        element.classList.add('hide');
+    }
+
+    function resetMoveAndScale(element) {
+        element.style.transitionDuration = null;
+        element.style.transform = getTransform({x: 100, y: 10}, 1.25);
+    }
+
     return {
         /**
          * Блок плавно появляется из прозрачного.
@@ -41,9 +82,26 @@ function animaster() {
          * @param duration — Продолжительность анимации в миллисекундах
          */
         fadeIn(element, duration) {
-            element.style.transitionDuration = `${duration}ms`;
-            element.classList.remove('hide');
-            element.classList.add('show');
+            return new Promise((resolve) => {
+                element.style.transitionDuration = `${duration}ms`;
+                element.classList.remove('hide');
+                element.classList.add('show');
+                setTimeout(resolve, duration);
+            });
+        },
+
+        /**
+         * Блок плавно исчезает из прозрачного.
+         * @param element — HTMLElement, который надо анимировать
+         * @param duration — Продолжительность анимации в миллисекундах
+         */
+        fadeOut(element, duration) {
+            return new Promise((resolve) => {
+                element.style.transitionDuration = `${duration}ms`;
+                element.classList.remove('show');
+                element.classList.add('hide');
+                setTimeout(resolve, duration);
+            });
         },
 
         /**
@@ -53,8 +111,11 @@ function animaster() {
          * @param translation — объект с полями x и y, обозначающими смещение блока
          */
         move(element, duration, translation) {
-            element.style.transitionDuration = `${duration}ms`;
-            element.style.transform = getTransform(translation, null);
+            return new Promise((resolve) => {
+                element.style.transitionDuration = `${duration}ms`;
+                element.style.transform = getTransform(translation, null);
+                setTimeout(resolve, duration);
+            });
         },
 
         /**
@@ -64,8 +125,56 @@ function animaster() {
          * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
          */
         scale(element, duration, ratio) {
-            element.style.transitionDuration = `${duration}ms`;
-            element.style.transform = getTransform(null, ratio);
+            return new Promise((resolve) => {
+                element.style.transitionDuration = `${duration}ms`;
+                element.style.transform = getTransform(null, ratio);
+                setTimeout(resolve, duration);
+            });
+        },
+
+        wait(duration) {
+            return new Promise((resolve) => {
+                setTimeout(resolve, duration);
+            });
+        },
+
+        async moveAndHide(element, duration) {
+            const translation = {x: 100, y: 20};
+            const durationMove = duration * 0.4;
+            const durationHide = duration * 0.6;
+
+            await this.move(element, durationMove, translation);
+            await this.fadeOut(element, durationHide);
+        },
+
+        async showAndHide(element, duration) {
+            const durationStep = duration / 3;
+
+            await this.fadeIn(element, durationStep);
+            await this.wait(durationStep);
+            await this.fadeOut(element, durationStep);
+        },
+
+        heartBeating(element) {
+            const ratio = 1.4;
+
+            let isRunning = true;
+
+            const animate = async () => {
+                await this.scale(element, 500, ratio);
+                await this.scale(element, 500, 1 / ratio);
+                if (isRunning) {
+                    setTimeout(animate, 0);
+                }
+            };
+
+            animate();
+
+            return {
+                stop: () => {
+                    isRunning = false;
+                },
+            };
         }
-    }
+    };
 }
